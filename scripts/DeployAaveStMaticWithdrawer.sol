7af7f872ecf0500e3fa8f29b02e0355a292bebf5
LucasWongC <wm111898@gmail.com>
2025-02-17 13:41:07 -0500

    added deploy scripts

---
+++ scripts/DeployAaveStMaticWithdrawer.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {EthereumScript} from "solidity-utils/contracts/utils/ScriptUtils.sol";
+import {AaveStMaticWithdrawer} from "src/asset-manager/stmatic-withdrawer/AaveStMaticWithdrawer.sol";
+
+contract DeployEthereum is EthereumScript {
+    // https://etherscan.io/address/0x9ee91F9f426fA633d227f7a9b000E28b9dfd8599
+    address public constant ST_MATIC =
+        0x9ee91F9f426fA633d227f7a9b000E28b9dfd8599;
+
+    function run() external broadcast {
+        bytes32 salt = "Aave StMatic Withdrawer";
+        new AaveStMaticWithdrawer{salt: salt}(ST_MATIC);
+    }
+}

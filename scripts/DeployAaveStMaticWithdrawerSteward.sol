c0d4b8429d9316a886e55f0bc2442ab40b8f4a55
LucasWongC <wm111898@gmail.com>
2025-02-17 14:50:55 -0500

    renamed on scripts

--- scripts/DeployAaveStMaticWithdrawerSteward.sol
+++ scripts/DeployAaveStMaticWithdrawerSteward.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import {EthereumScript} from "solidity-utils/contracts/utils/ScriptUtils.sol";
-import {AaveStMaticWithdrawer} from "src/asset-manager/stmatic-withdrawer/AaveStMaticWithdrawer.sol";
+import {AaveStMaticWithdrawerSteward} from "src/asset-manager/stmatic-withdrawer/AaveStMaticWithdrawerSteward.sol";
 
 contract DeployEthereum is EthereumScript {
     // https://etherscan.io/address/0x9ee91F9f426fA633d227f7a9b000E28b9dfd8599
@@ -12,6 +12,6 @@ contract DeployEthereum is EthereumScript {
 
     function run() external broadcast {
         bytes32 salt = "Aave StMatic Withdrawer";
-        new AaveStMaticWithdrawer{salt: salt}(ST_MATIC);
+        new AaveStMaticWithdrawerSteward{salt: salt}(ST_MATIC);
     }
 }

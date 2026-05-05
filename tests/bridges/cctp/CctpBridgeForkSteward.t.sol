c60fc0e1f4ccb2434085a06d20b0cb35abf7ac40
Juan Ignacio Ubeira <juani@balancer.finance>
2026-05-05 14:51:01 -0300

    WIP: refactor tests to fork only when necessary.

--- tests/bridges/cctp/CctpBridgeForkSteward.t.sol
+++ tests/bridges/cctp/CctpBridgeForkSteward.t.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {Test} from 'forge-std/Test.sol';
+
 import {AaveV3Arbitrum} from 'aave-address-book/AaveV3Arbitrum.sol';
 import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
 import {IAccessControl} from 'openzeppelin-contracts/contracts/access/IAccessControl.sol';
-import {Test} from 'forge-std/Test.sol';
+import {ERC20Mock} from 'openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol';
 import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 import {IWithGuardian} from 'solidity-utils/contracts/access-control/interfaces/IWithGuardian.sol';
 
 import {CctpBridgeSteward} from 'src/bridges/cctp/CctpBridgeSteward.sol';
 import {ICctpBridgeSteward} from 'src/bridges/cctp/interfaces/ICctpBridgeSteward.sol';
+import {ITokenMessengerV2} from 'src/bridges/cctp/interfaces/ITokenMessengerV2.sol';
+import {IMessageTransmitterV2} from 'src/bridges/cctp/interfaces/IMessageTransmitterV2.sol';
 import {CctpConstants} from 'src/bridges/cctp/CctpConstants.sol';
 
 contract CctpBridgeStewardForkTestBase is Test {
   CctpBridgeSteward public bridge;
-  IERC20 public usdc;
+  IERC20 public usdc = new ERC20Mock();
+  address public tokenMessenger = makeAddr('tokenMessenger');
   address public owner = makeAddr('owner');
   address public guardian = makeAddr('guardian');
   address public collector = address(AaveV3Arbitrum.COLLECTOR);
@@ -25,14 +30,7 @@ contract CctpBridgeStewardForkTestBase is Test {
 
   function _deployBridge() internal returns (CctpBridgeSteward) {
     return
-      new CctpBridgeSteward(
-        CctpConstants.ARBITRUM_TOKEN_MESSENGER,
-        CctpConstants.ARBITRUM_USDC,
-        owner,
-        guardian,
-        collector,
-        receiver
-      );
+      new CctpBridgeSteward(tokenMessenger, address(usdc), owner, guardian, collector, receiver);
   }
 
   function _addressToBytes32(address addr) internal pure returns (bytes32) {
@@ -44,30 +42,36 @@ contract CctpBridgeStewardForkTestBase is Test {
   }
 
   function setUp() public virtual {
-    string memory rpcUrl = vm.envOr('RPC_ARBITRUM', string(''));
-    vm.createSelectFork(rpcUrl);
-
-    usdc = IERC20(CctpConstants.ARBITRUM_USDC);
+    try vm.activeFork() returns (uint256) {
+      // If we are on a fork, continue.
+    } catch {
+      // If not, mock the necessary calls to deploy the bridge correctly.
+      address localMessageTransmitter = makeAddr('localMessageTransmitter');
+      vm.mockCall(
+        tokenMessenger,
+        abi.encodeCall(ITokenMessengerV2(tokenMessenger).localMessageTransmitter, ()),
+        abi.encode(localMessageTransmitter)
+      );
+      vm.mockCall(
+        localMessageTransmitter,
+        abi.encodeCall(IMessageTransmitterV2(localMessageTransmitter).localDomain, ()),
+        abi.encode(CctpConstants.ARBITRUM_DOMAIN)
+      );
+    }
 
     bridge = _deployBridge();
 
-    bytes32 fundsAdminRole = AaveV3Arbitrum.COLLECTOR.FUNDS_ADMIN_ROLE();
-    vm.prank(AaveV3Arbitrum.ACL_ADMIN);
-    IAccessControl(collector).grantRole(fundsAdminRole, address(bridge));
+    _fundCollector(AMOUNT);
   }
 
-  function _bridge(
-    uint256 maxFee,
-    ICctpBridgeSteward.TransferSpeed speed
-  ) internal {
-    _fundCollector(AMOUNT);
+  function _bridge(uint256 maxFee, ICctpBridgeSteward.TransferSpeed speed) internal {
     uint256 collectorBalanceBefore = usdc.balanceOf(collector);
 
     vm.expectEmit();
     emit ICctpBridgeSteward.Bridge(
       address(usdc),
       CctpConstants.ETHEREUM_DOMAIN,
-      address(AaveV3Ethereum.COLLECTOR),
+      receiver,
       AMOUNT,
       speed
     );
@@ -84,7 +88,49 @@ contract CctpBridgeStewardForkTestBase is Test {
   }
 }
 
-contract BridgeEvmTest is CctpBridgeStewardForkTestBase {
+contract BridgeFailuresTest is CctpBridgeStewardForkTestBase {
+  function test_revertsIf_callerNotOwnerOrGuardian() public {
+    vm.startPrank(alice);
+    vm.expectRevert(
+      abi.encodeWithSelector(IWithGuardian.OnlyGuardianOrOwnerInvalidCaller.selector, alice)
+    );
+    bridge.bridge(
+      AMOUNT,
+      0,
+      ICctpBridgeSteward.TransferSpeed.Fast
+    );
+    vm.stopPrank();
+  }
+
+  function test_revertsIf_zeroAmount() public {
+    vm.startPrank(owner);
+    vm.expectRevert(ICctpBridgeSteward.InvalidZeroAmount.selector);
+    bridge.bridge(
+      0,
+      0,
+      ICctpBridgeSteward.TransferSpeed.Fast
+    );
+    vm.stopPrank();
+  }
+}
+
+contract BridgeTest is CctpBridgeStewardForkTestBase {
+  function setUp() public override {
+    string memory rpcUrl = vm.envOr('RPC_ARBITRUM', string(''));
+    vm.createSelectFork(rpcUrl);
+
+    usdc = IERC20(CctpConstants.ARBITRUM_USDC);
+    collector = address(AaveV3Arbitrum.COLLECTOR);
+    tokenMessenger = CctpConstants.ARBITRUM_TOKEN_MESSENGER;
+    receiver = address(AaveV3Arbitrum.COLLECTOR);
+
+    super.setUp();
+
+    bytes32 fundsAdminRole = AaveV3Arbitrum.COLLECTOR.FUNDS_ADMIN_ROLE();
+    vm.prank(AaveV3Arbitrum.ACL_ADMIN);
+    IAccessControl(collector).grantRole(fundsAdminRole, address(bridge));
+  }
+
   function test_bridge_fast() public {
     _bridge(
       AMOUNT / 100,
@@ -93,7 +139,6 @@ contract BridgeEvmTest is CctpBridgeStewardForkTestBase {
   }
 
   function test_bridge_fast_guardian() public {
-    _fundCollector(AMOUNT);
     uint256 collectorBalanceBefore = usdc.balanceOf(collector);
 
     vm.expectEmit();
@@ -126,36 +171,19 @@ contract BridgeEvmTest is CctpBridgeStewardForkTestBase {
       ICctpBridgeSteward.TransferSpeed.Standard
     );
   }
-
-  function test_revertsIf_callerNotOwnerOrGuardian() public {
-    vm.startPrank(alice);
-    vm.expectRevert(
-      abi.encodeWithSelector(IWithGuardian.OnlyGuardianOrOwnerInvalidCaller.selector, alice)
-    );
-    bridge.bridge(
-      AMOUNT,
-      0,
-      ICctpBridgeSteward.TransferSpeed.Fast
-    );
-    vm.stopPrank();
-  }
-
-  function test_revertsIf_zeroAmount() public {
-    vm.startPrank(owner);
-    vm.expectRevert(ICctpBridgeSteward.InvalidZeroAmount.selector);
-    bridge.bridge(
-      0,
-      0,
-      ICctpBridgeSteward.TransferSpeed.Fast
-    );
-    vm.stopPrank();
-  }
 }
 
 contract ConstructorTest is CctpBridgeStewardForkTestBase {
   function test_revertsIf_constructorTokenMessengerZero() public {
     vm.expectRevert(ICctpBridgeSteward.InvalidZeroAddress.selector);
-    new CctpBridgeSteward(address(0), CctpConstants.ETHEREUM_USDC, owner, guardian, collector, receiver);
+    new CctpBridgeSteward(
+      address(0),
+      CctpConstants.ETHEREUM_USDC,
+      owner,
+      guardian,
+      collector,
+      receiver
+    );
   }
 
   function test_revertsIf_constructorUsdcZero() public {
@@ -217,7 +245,11 @@ contract RescuableTest is CctpBridgeStewardForkTestBase {
     bridge.rescueToken(address(usdc));
 
     assertEq(usdc.balanceOf(address(bridge)), 0, 'Rescue bridge should have no USDC left');
-    assertEq(usdc.balanceOf(collector), collectorBalanceBefore + AMOUNT, 'Collector should receive rescued USDC');
+    assertEq(
+      usdc.balanceOf(collector),
+      collectorBalanceBefore + AMOUNT,
+      'Collector should receive rescued USDC'
+    );
   }
 
   function test_sendEthToBridge_reverts() public {
@@ -246,9 +278,8 @@ contract RescuableTest is CctpBridgeStewardForkTestBase {
   }
 
   function test_maxRescue_returnsFullBalance() public {
-    CctpBridgeSteward rescueBridge = _deployBridge();
-    deal(address(usdc), address(rescueBridge), AMOUNT);
+    deal(address(usdc), address(bridge), AMOUNT);
 
-    assertEq(rescueBridge.maxRescue(address(usdc)), AMOUNT);
+    assertEq(bridge.maxRescue(address(usdc)), AMOUNT);
   }
 }
